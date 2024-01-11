d26a4c34f01ca0a81a4b777018d7350c7128b9c3
Luiz Serão <luiz.serao@hotmail.com>
2024-01-11 13:59:59 -0300

    Tentativa de correção no erro de login

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -8,7 +8,7 @@ export const UserStorage = ({children}) => {
   const [data, setData] = React.useState(null);
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState(null);
+  const [error, setError] = React.useState(false);
   const navigate = useNavigate();
 
   const userLogout = React.useCallback(async function() {
